ba10b99f207401509d4fa9e563586cd79b159cbd
LuisFernando <luisfer.prod@gmail.com>
2020-05-03 17:58:36 +0200

    trabajo 03/05/2020_expresion regular tarjeta de credito (fecha, codigo y numero)

--- src/app/credito/credito.component.ts
+++ src/app/credito/credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-credito',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
 
@@ -52,9 +53,46 @@ export class CreditoComponent implements OnInit {
     var dia = d.getDate();
     var mes = d.getMonth();
     var year = d.getFullYear();
-    // console.log(year);
-    // console.log(mes);
-    // console.log(dia);
+
+    mes++;
+
+    console.log(fechaCambiada);
+    console.log(year);
+    console.log(mes);
+    console.log(dia);
+
+     if (fechaCambiada[0]<year || ( fechaCambiada[0]==year && fechaCambiada[1]<mes )
+     || (  fechaCambiada[0]==year && fechaCambiada[1]==mes &&  fechaCambiada[2]<dia ) ) {
+
+        sweetAlert('Su tarjeta de crédito esta caducada. Por favor, renuévela');
+
+     } else {
+
+        if (this.codigo!=null) {
+
+          var length = Math.log(this.codigo) * Math.LOG10E + 1 | 0;
+          console.log(length);
+
+           if ( length<3 || length>3 ) {
+
+            sweetAlert('El código de seguridad no mide 3 dígitos');
+
+          } else {
+
+            var patron=/^\d{4}\-\d{4}\-\d{4}\-\d{4}$/;
+
+            if (patron.test(this.tarjeta)){
+              this.router.navigate(['/confirmacion']);
+            } else {
+              sweetAlert('El número de tarjeta introducido no es el correcto. Debe tener el formato xxxx-xxxx-xxxx-xxxx');
+            }
+
+          }
+
+        } else {
+          sweetAlert('El código de seguridad está vacío');
+        }
+     }
   }
 
 }
